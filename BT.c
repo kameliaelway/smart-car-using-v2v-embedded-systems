19d2da481f2fe5cd330cabd4c47cf8c225b8d72a
kameliaelway <71229445+kameliaelway@users.noreply.github.com>
2022-02-09 17:26:15 +0200

    Add files via upload

---
+++ BT.c
@@ -0,0 +1,29 @@
+/*
+ * BT.c
+ *
+ *  Created on: 4 May 2021
+ *      Author: DELL
+ */
+#include "BT.h"
+
+void BT_Init(long baudrate)
+{
+	Uart_Init(baudrate);
+}
+void BT_Send_Byte(char Data)
+{
+	Uart_Send_Byte(Data);
+}
+void BT_Send_String(char * String)
+{
+	Uart_Send_String(String);
+}
+char BT_Rec_Byte(void)
+{
+	return Uart_Rec_Byte();
+}
+void BT_Rec_String(char * String)
+{
+	Uart_Rec_String(String);
+}
+
---
+++ PWM.h
@@ -0,0 +1,16 @@
+/*
+ * PWM.h
+ *
+ *  Created on: Oct 5, 2016
+ *      Author: ahmed
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include <avr/io.h>
+
+extern void PWM_init(void);
+extern void PWM_Duty_Cycle(unsigned char Duty_Cycle);
+
+#endif /* PWM_H_ */
